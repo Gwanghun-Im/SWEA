d8f6e2fa1662c1a809fd712ba93c0cc0c39e5aca
Gwanghun Im <lgh82229047@gmail.com>
2021-01-22 21:33:51 +0900

    1215 swea

---
+++ 1215.py
@@ -0,0 +1,24 @@
+for t in range(10):
+    word = []
+    cnt = 0
+
+    n = int(input())
+    for _ in range(8):
+        word.append(input())
+    word_T = [list(x) for x in zip(*word)]
+
+    for i in range(8):
+        for j in range(8):
+
+            temp = word[i][j:j+n]
+            if len(temp) < n:
+                continue
+
+            if temp == temp[::-1]:
+                cnt += 1
+
+            temp_ = word_T[i][j:j+n]
+            if temp_ == temp_[::-1]:
+                cnt += 1
+
+    print(f'#{t+1} {cnt}')
